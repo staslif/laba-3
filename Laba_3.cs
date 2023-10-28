fad079d0f08b601c306aa5379e8b2c1797cb7550
staslif <149203375+staslif@users.noreply.github.com>
2023-10-28 13:01:06 +0300

    Add files via upload

---
+++ Laba_3.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите A, B");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int S = 0;
+for (int i = A; i <= B; i++)
+{
+    if ((i % 5 == 0) && (i % 7 == 0))
+    {
+        S += i;
+    }
+}
+Console.WriteLine(S);
+
